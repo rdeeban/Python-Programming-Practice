e1857f4afa9c1e7d1e7f4ecc6867df13146ff20d
Deeban Ramalingam <rdeeban@gmail.com>
2023-08-28 01:48:52 -0700

    Worked on Programming Interview Problem 1

--- main/programming_interview_problems.py
+++ main/programming_interview_problems.py
@@ -5,6 +5,18 @@ class ProgrammingInterviewProblems:
     """
     @staticmethod
     def findMissingNumber(arr):
+        is_number_missing = False
+        for i in range(100):
+            if i == 99 and len(arr) == 99:
+                is_number_missing = True
+                break
+
+            if i + 1 < arr[i]:
+                is_number_missing = True
+
+        if not is_number_missing:
+            raise ValueError('Invalid input: there are no numbers missing.')
+
         for i in range(99):
             if i + 1 < arr[i]:
                 return i + 1
